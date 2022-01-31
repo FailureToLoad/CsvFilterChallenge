0af8d7d5afb4c7d90490a744ca2a85d851b80e7b
dmason <davejmason@outlook.com>
2022-01-30 23:44:05 -0500

    adds test for valid transition from RequestNewFile

--- CsvReader.Tests/IngressTests.cs
+++ CsvReader.Tests/IngressTests.cs
@@ -8,13 +8,26 @@ namespace CsvReader.Tests;
 public class IngressTests
 {
     private const string MissingFilePath = "garbage";
+    private const string ValidFilePath = "valid.csv";
+
+    
 
     [Test]
-    public void When_UserInputsMissingFilePath_Then_ReturnToStartingPoint()
+    public void When_UserInputsMissingFilePath_Then_RequestNewFile()
     {
         Console.SetIn(new StringReader(MissingFilePath));
         IContext context = new RequestFilePathContext();
         context = context.Transition();
         Assert.True(context is RequestFilePathContext);
     }
+    
+    [Test]
+    public void When_UserInputsValidFilePath_Then_AdvanceToFileProcessing()
+    {
+        Console.SetIn(new StringReader(ValidFilePath));
+        IContext context = new RequestFilePathContext();
+        context = context.Transition();
+        Assert.True(context is FileProcessingContext);
+    }
+    
 }
